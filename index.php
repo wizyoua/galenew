1900a92392bde8aed46fd1c1abd271fe2d266ef8
Gianni Zamora <giannizcool@gmail.com>
2017-03-22 17:05:07 -0700

    new

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gale Leadership</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!-- JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container wrap-cont" >
+	<div class="row first-area">
+		<div class="col-md-12">
+			<nav class="navbar navbar-inverse">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>                        
+			      </button>			      
+			    </div>
+			    <div class="collapse navbar-collapse" id="myNavbar">
+			      <ul class="nav navbar-nav">
+			        <li class="active"><a href="#">Meet Amba</a></li>
+			        <li><a href="#">What We Do</a></li>
+			        <li><a href="#">Programs</a></li>
+			        <li><a href="#">Our Clients</a></li>
+			        <li><a href="#">Contact</a></li>
+			        <li><a href="#">Blog</a></li>
+			        <li><a href="#">Ny Account</a></li>
+			        <li><form class="navbar-form ">
+			        <div class="form-group">
+			          <input type="text" class="form-control" placeholder="Search">
+			        </div>
+			        <button type="submit" class="btn btn-default search-btn"><span class="glyphicon glyphicon-search"></span></button>
+			      </form></li>
+			      </ul>			      
+			    </div>
+			  </div>
+			</nav>
+		</div>
+		<div class="col-md-12 logo-area" >
+			<img src="images/general/logo.png" alt="Gale Leadership">
+		</div>
+		<div class="col-md-12 logo-text-holder">
+			<h3 class="logo-text">Awakening heart<br> spirit and joy in worklife</h3>
+		</div>
+	</div>
+	<div class="row second-area">
+		<div class="col-md-12 second-area-bkg" >
+			<img src="images/general/sec2.jpg" class="second-area-bkg-img">
+		</div>
+	</div>
+	<div class="row third-area">
+		<div class="col-md-12">
+			<div class="row">
+				<div class=" col-lg-4 col-md-4 left">
+					<p class="left-text">What <br>We do</p>
+				</div>
+				<div class=" col-lg-8 col-md-8 right">
+					<div class="right-div">
+						<p class="right-div-1">We provide personal and business transformation.</p>
+						<p class="right-div-2">We provide inquiries into potent questions about meaning in life, freedom, the source of trust, relationships that work, and the source of power and effectiveness. We work with people to disappear, or eliminate, ways of being and acting that do not contribute to their lives, and we illuminate pathways for enjoying a bigger life, a fuller life. We invite people to engage in the meeting of their heart's desire and what is calling to them from the world; in other words, to be able to pursue a joyful inheritance, which is our birthright, including in the workplace...</p>
+						<button class="right-div-3">Learn More</button>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row fourth-area" >
+		<div class="col-md-12 fourth-area-holder" >
+			<div class="col-lg-6 col-md-6 fourth-area-left"><p class="fourth-left-text">program<br>offerings</p></div>
+			<div class="col-lg-6 col-md-6 fourth-area-right"> 
+				<p class="right-p"><a href="#" class="right-a">The Heart of Leadership</a><br>
+				Dates: October  4, 5, 6 and 19, 20 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Being: Designing Your Life</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Making a Difference: Being of Service, Coaching Others, Enrolling Others, Speaking UP</a></p>
+				<p class="right-p"><a href="#" class="right-a">Leading Groups Effectively</a><br>
+				Dates: October 3, 4, 5, 6, 9, 10, 11 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Accomplishment</a><br>
+				Dates: October  4, 5, 6 and 19, 20 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">Joy of Being II: Faith, Fierceness, and Fluidity</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Living III: Vulnerability, Equanimity, Intuition</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Being a Life Worth Living IV: Surrender, Open Heartedness, Intentionality</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Leadership for Business</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Listening: Creating Workability</a></p>
+				<p class="right-p"><a href="#" class="right-a">Listening from Love: Passion, Presence, Play</a></p>
+				<p class="right-p"><a href="#" class="right-a">Designing a Vision: Living From a Meaningful Future</a></p>
+				<p class="right-p"><a href="#" class="right-a">Specially Designed Programs for Businesses</a></p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">5</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">6</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">7</div>
+	</div>
+	
+</div>
+
+</body>
+</html>
