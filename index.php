7bea6d0690fd3b11c0625ebf0f404131d88f3ea4
Gianni Zamora <giannizcool@gmail.com>
2017-03-27 10:33:24 -0700

    new

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 
 <!-- CSS -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+
 
 <!-- JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -34,10 +36,10 @@
 			        <li><a href="#">Our Clients</a></li>
 			        <li><a href="#">Contact</a></li>
 			        <li><a href="#">Blog</a></li>
-			        <li><a href="#">Ny Account</a></li>
+			        <li><a href="#">My Account</a></li>
 			        <li><form class="navbar-form ">
 			        <div class="form-group">
-			          <input type="text" class="form-control" placeholder="Search">
+			          <input type="text" class="form-control nav-search" placeholder="Search">
 			        </div>
 			        <button type="submit" class="btn btn-default search-btn"><span class="glyphicon glyphicon-search"></span></button>
 			      </form></li>
@@ -99,16 +101,100 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12">5</div>
+		<div class="col-md-12 fifth-area-holder">
+			<img src="images/general/sec6.jpg" alt="Gale Leadership" class="fifth-area-img"> 
+			<div style="width: 100%; position: absolute; top:25%; text-align: center;"><p style="font-size: 64px; color: #006067; letter-spacing: 5px; font-weight: bold; text-transform: uppercase; ">Inspired</p></div>
+			<div style="height: auto; background-color: #d9e6e6; margin: 0px 5%; text-align: center;  position: relative; bottom: 100px;padding: 5% 0px; ">
+				<div class='row'>
+				    <div class='col-md-12'>
+				      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+				        <!-- Bottom Carousel Indicators -->
+				        <ol class="carousel-indicators">
+				          <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+				          <li data-target="#quote-carousel" data-slide-to="1"></li>
+				          <li data-target="#quote-carousel" data-slide-to="2"></li>
+				        </ol>
+				        
+				        <!-- Carousel Slides / Quotes -->
+				        <div class="carousel-inner">
+				        
+				          <!-- Quote 1 -->
+				          <div class="item active">
+				            <blockquote>
+				              <div class="row">				                
+				                <div class="col-sm-12">
+				                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
+				                  <small>Someone famous</small>
+				                </div>
+				              </div>
+				            </blockquote>
+				          </div>
+				          <!-- Quote 2 -->
+				          <div class="item">
+				            <blockquote>
+				              <div class="row">				                
+				                <div class="col-sm-12">
+				                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+				                  <small>Someone famous</small>
+				                </div>
+				              </div>
+				            </blockquote>
+				          </div>
+				          <!-- Quote 3 -->
+				          <div class="item">
+				            <blockquote>
+				              <div class="row">				               
+				                <div class="col-sm-12">
+				                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
+				                  <small>Someone famous</small>
+				                </div>
+				              </div>
+				            </blockquote>
+				          </div>
+				        </div>
+				        
+				        <!-- Carousel Buttons Next/Prev -->
+				        <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+				        <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+				      </div>                          
+				    </div>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-12">6</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">7</div>
+	<div class="row fourth-area" >
+		<div class="col-md-12 fourth-area-holder" >
+			<div class="col-lg-6 col-md-6 fourth-area-left"><p class="fourth-left-text">our<br>clients<br> speak</p></div>
+			<div class="col-lg-6 col-md-6 fourth-area-right"> 
+				<p class="right-p"><a href="#" class="right-a">The Heart of Leadership</a><br>
+				Dates: October  4, 5, 6 and 19, 20 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Being: Designing Your Life</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Making a Difference: Being of Service, Coaching Others, Enrolling Others, Speaking UP</a></p>
+				<p class="right-p"><a href="#" class="right-a">Leading Groups Effectively</a><br>
+				Dates: October 3, 4, 5, 6, 9, 10, 11 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Accomplishment</a><br>
+				Dates: October  4, 5, 6 and 19, 20 of 2017</p>
+				<p class="right-p"><a href="#" class="right-a">Joy of Being II: Faith, Fierceness, and Fluidity</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Living III: Vulnerability, Equanimity, Intuition</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Joy of Being a Life Worth Living IV: Surrender, Open Heartedness, Intentionality</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Leadership for Business</a></p>
+				<p class="right-p"><a href="#" class="right-a">The Heart of Listening: Creating Workability</a></p>
+				<p class="right-p"><a href="#" class="right-a">Listening from Love: Passion, Presence, Play</a></p>
+				<p class="right-p"><a href="#" class="right-a">Designing a Vision: Living From a Meaningful Future</a></p>
+				<p class="right-p"><a href="#" class="right-a">Specially Designed Programs for Businesses</a></p>
+			</div>
+		</div>
 	</div>
 	
 </div>
-
+<script type="text/javascript">
+	$(document).ready(function() {
+	  //Set the carousel options
+	  $('#quote-carousel').carousel({
+	    pause: true,
+	    interval: 4000,
+	  });
+	});
+</script>
 </body>
 </html>
